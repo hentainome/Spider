96cd1748c9cd3c90d79d3722dcb84cc4e43b7dbc
hentainome <kaisen341125@gmail.com>
2017-05-10 14:47:05 +0800

    just login by python3.5

---
+++ Github/fuckgit.py
@@ -0,0 +1,75 @@
+import requests
+import http.cookiejar
+import re
+from lxml import etree
+
+class GithubLogin(object):
+	
+	def __init__(self):
+		self.headers={
+		 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Accept-Encoding':'gzip, deflate, sdch, br',
+		'Accept-Language': 'zh-CN,zh;q=0.8,ja;q=0.6,en;q=0.4',
+		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+		}
+
+		self.login_url = 'https://github.com/login'
+		self.post_url = 'https://github.com/session'
+		self.logined_url = 'https://github.com/settings/profile'
+
+		self.session = requests.Session()
+		self.session.cookies = http.cookiejar.LWPCookieJar(filename='github_cookie')
+
+
+	def load_cookie(self):
+		try:
+			self.session.cookies.load(ignore_discard=True)
+		except:
+			print('cookie 不成功')
+	
+
+	def get_param(self):
+		response = self.session.get(self.login_url, headers=self.headers,)
+		selector = etree.HTML(response.text)
+		field_one = selector.xpath('//form/input[1]/@value')
+		print(field_one)
+		return field_one
+		pass
+
+	def post_param(self,email,password):
+		post_data = {
+			'commit': 'Sign in',
+            'utf8': '✓',
+            'authenticity_token': self.get_param()[0],
+            'login':email,
+            'password': password
+		}
+		response = self.session.post(self.post_url,data=post_data,headers=self.headers,)
+		print(response.status_code)
+		self.session.cookies.save()
+
+
+	def bool_login(self):
+		self.load_cookie()
+		response = self.session.get(self.logined_url, headers=self.headers,)
+		print(response.status_code)
+		selector = etree.HTML(response.text)
+		print(response.text)
+		flag = selector.xpath('//div[@class="column two-thirds"]/dl/dt/label/text()')
+		print(u'个人设置Profile包括: %s'%flag)
+		pass
+
+if __name__ == "__main__":
+	Github = GithubLogin()
+	Github.post_param(email='kaisen341125@gmail.com',password='naruto341125')
+	Github.bool_login()
+
+
+
+#############################
+#							#
+# 							#
+#			sucess			#
+#							#
+# 							#
+#############################
---
+++ Pixiv/fuckpixiv.py
@@ -0,0 +1,58 @@
+import requests
+import http.cookiejar
+from lxml import etree
+
+
+class PixivLogin(object):
+	def __init__(self):
+		self.headers={
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Accept-Encoding':'gzip, deflate, sdch, br',
+		'Accept-Language': 'zh-CN,zh;q=0.8,ja;q=0.6,en;q=0.4',
+		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+		}
+		self.get_url = 'https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index'
+		self.login_url='https://accounts.pixiv.net/api/login?lang=zh'
+		self.bookmark_url='https://www.pixiv.net/bookmark.php'
+		self.session = requests.Session()
+		self.session.cookies = http.cookiejar.LWPCookieJar(filename='pixiv_cookies')
+
+	def loadcookie(self):
+		try:
+			self.session.cookies.load(ignore_discard=True)
+		except:
+			print('cookie 保存失败')
+
+	def get_key(self):
+		reponse = self.session.get(self.get_url,headers=self.headers)
+		selector = etree.HTML(reponse.text)
+		post_key = selector.xpath('//form/input[1]/@value')
+		print(post_key)
+		return post_key
+
+	def post_value(self,email,password):
+		self.data ={
+		'pixiv_id':email,
+		'password':password,
+		'captcha':'',
+		'g_recaptcha_response':'',
+		'post_key': self.get_key()[0],
+		'source':'pc',
+		'ref':'wwwtop_accounts_index',
+		'return_to':'http://www.pixiv.net/'
+		}
+		reponse=self.session.post(self.login_url,data=self.data,headers=self.headers)
+		print(reponse.status_code)
+		self.session.cookies.save()
+
+	def book(self):
+		reponse = self.session.get(self.bookmark_url,headers=self.headers)
+		print(reponse.status_code)
+		selector = etree.HTML(reponse.text)
+		photolist = selector.xpath('//li/a/@href')
+		print(photolist)
+
+if __name__ == "__main__":
+	Pixiv = PixivLogin()
+	Pixiv.post_value(email='1574610621@qq.com',password='naruto341125')
+	Pixiv.book()
